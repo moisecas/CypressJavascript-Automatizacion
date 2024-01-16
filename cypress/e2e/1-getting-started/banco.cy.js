ac66a30c6d00914ed9616c673e8fcd3231789056
unknown <moisesjuniorcas20@gmail.com>
2024-01-16 15:46:54 -0500

    first commit

---
+++ cypress/e2e/1-getting-started/banco.cy.js
@@ -0,0 +1,9 @@
+//entrar a esta pagina https://www.bancodeoccidente.com.co/wps/portal/banco-de-occidente/bancodeoccidente/
+
+describe('Banco de Occidente', () => { 
+
+    it('Ingresar a la pagina', () => {
+        cy.visit('https://www.bancodeoccidente.com.co/wps/portal/banco-de-occidente/bancodeoccidente/')
+    })
+    
+})
