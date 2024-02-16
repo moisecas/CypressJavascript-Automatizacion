142a33cb7585918857cd4e448bd6c0af0b9f19c3
unknown <moisesjuniorcas20@gmail.com>
2024-02-16 15:45:21 -0500

    empezando test personal

--- cypress/e2e/1-getting-started/banco.cy.js
+++ cypress/e2e/1-getting-started/banco.cy.js
@@ -4,6 +4,10 @@ describe('Banco de Occidente', () => {
 
     it('Ingresar a la pagina', () => {
         cy.visit('https://www.bancodeoccidente.com.co/wps/portal/banco-de-occidente/bancodeoccidente/')
+        cy.get('.animated > .item > .caption > .button').click({ multiple: true });
+
+        cy.get('.animated > .item > .caption > .button').click()
+        cy.get('#request-credit').click()
     })
     
 })
